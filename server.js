3029580ef86bac75709f7321784a862448e73f7a
BubbaMachina <bubbasamuels@gmail.com>
2018-10-05 17:55:09 -0400

    test2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(__dirname+"dist/tliCalculator"));
 
-app.get('*',function(req,res){
+app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+ '/dist/tliCalculator/index.html'));
 });
 
